2dc07d501f42f4f1c8c34d2fb97d4d3dfe082418
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 21:52:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,1 @@
-string = "Hello my name is Harleigh"
-def reverse_each_word(string)
-  sentence = string.split(" ")
-  sentence.each do |word|
-    word.reverse!
-  end
- sentence = sentence.join(" ")
-end
 
-def reverse_each_word(string)
-
- sentence = string.split
- sentence.collect do |word|
-   word.reverse!
-  end
-sentence.join(" ")
-
-end
