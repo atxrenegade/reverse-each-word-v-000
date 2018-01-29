bbbc360426bd9036416aaf9782020e0456a14a4d
Harleigh Abel <harleighabel@gmail.com>
2018-01-29 22:13:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,11 @@ def reverse_each_word(sentence)
   sentence = sentence.reverse
   sentence = sentence.join(" ")
 end
+
+def reverse_each_word(sentence)
+  
+  sentence = sentence.split
+  sentence.collect {|word| word.reverse}
+  sentence = sentence.reverse
+  sentence = sentence.join(" ")
+end
